5fc185ce52cc99004a77db50f55d9638c223be8b
bigbuckalex <bigbuckalex@gmail.com>
2019-05-08 19:28:55 -0400

    added mongoDB connection to server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://alex:alex123@cluster0-8dccq.mongodb.net/test?retryWrites=true'
+}
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }

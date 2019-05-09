b6a2222ba3919c566d27eea6fa163e5ecb77267e
bigbuckalex <bigbuckalex@gmail.com>
2019-05-09 13:57:24 -0400

    added route to get random clue and route to delete all final jeopardy clues

--- routes/api/clues.js
+++ routes/api/clues.js
@@ -3,14 +3,28 @@ const router = express.Router();
 
 const Clues = require('../../models/Clues');
 
+//GET a random clue
+router.get('/random', (req, res) => {
+  Clues.find()
+    .then(clues => {
+      const randomClueNum = Math.floor(Math.random() * clues.length);
+      res.json(clues[randomClueNum]);
+    });
+});
+
 //GET a clue from a specific category with a specific dollar value
 router.get('/', (req, res) => {
-  console.log(req.body);
   Clues.find(
     {
       category: req.body.category,
       value: req.body.value
     })
+    .then(clues => res.json(clues));
+});
+
+//DELETE all clues in final jeopardy
+router.delete('/', (req, res) => {
+  Clues.find({round:'Final Jeopardy!'})
     .then(clues => res.json(clues));
 });
 
